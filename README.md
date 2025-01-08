1822d7b79aa8f3a07212d06996468348be3415f1
Matthew Levy <pixel1313@gmail.com>
2025-01-07 18:19:03 -0600

    Add Survey entity

--- README.md
+++ README.md
@@ -8,19 +8,33 @@ manually edit one of the vendor files after doing a composer install.
 
 line 20633 has a comment section that looks something like:
 ```js
- * // Asynchronous loading
- * import { LazyMotion, m } from "framer-motion"
- *
- * function App() {
- *   return (
- *     <LazyMotion features={() => import('./path/to/domAnimations')}>
- *       <m.div animate={{ scale: 2 }} />
- *     </LazyMotion>
- *   )
- * }
+// Asynchronous loading
+import { LazyMotion, m } from "framer-motion"
+
+function App() {
+  return (
+    <LazyMotion features={() => import('./path/to/domAnimations')}>
+      <m.div animate={{ scale: 2 }} />
+    </LazyMotion>
+  )
+}
 ```
 
 The `import` command is being picked up by asset mapper, even though its in a
 docblock because they use regex to generate the importmap. This works great when
 javascript developers actually issue minimised code, and works like total crap
 otherwise. Change the word `import` to anything else. I went with `mimport`.
+
+# Setup
+
+```shell
+# install php dependencies.
+composer install
+
+# create database
+symfony console doctrine:database:create
+
+# apply database schema migrations.
+symfony console doctrine:migrations:migrate
+
+```
---
+++ src/Entity/Survey.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\SurveyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SurveyRepository::class)]
+#[ApiResource]
+class Survey
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function isPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setPublished(bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
